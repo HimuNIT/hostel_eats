f973d482f25eee2a73aeaef5270ec9bc5da672cb
HimuNIT <himwebd96@gmail.com>
2024-08-05 16:34:46 +0530

    Update login.js

--- server/controllers/login.js
+++ server/controllers/login.js
@@ -56,8 +56,8 @@ exports.userLogin = async (req, res) => {
       const options={
    //expires:new Date (Date.now()+3*24*60*60*1000),
     httpOnly:true,  //client side access nahi hoga
-    sameSite:'Lax',   //Local host -> None and for network Lax
-   // secure:true,    
+    sameSite:'None',   //Local host -> None and for network Lax
+    secure:true,    
       }
       res.cookie("token",token,options).status(200).json({
         success: true,
@@ -333,8 +333,8 @@ exports.popularCanteens=async(req,res)=>{
 exports.logout=async(req,res)=>{
  const options = {
    httpOnly: true,
-   sameSite: "Lax",
-   //secure: true,
+   sameSite: "None",
+    secure: true,
    expires: new Date(Date.now() - 1000),
  };
   res.cookie("token", "", options);
